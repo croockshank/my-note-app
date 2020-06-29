5dc6528f626dec89d57603074eef2db4f1d461e3
Genadi Dharma <genadi.alba@gmail.com>
2020-06-29 17:40:39 +0800

    [ADDED] consumer app added

---
+++ consumerapp/src/main/java/com/genadidharma/consumerapp/helper/MappingHelper.kt
@@ -0,0 +1,36 @@
+package com.genadidharma.consumerapp.helper
+
+import android.database.Cursor
+import com.genadidharma.consumerapp.db.DatabaseContract
+import com.genadidharma.consumerapp.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(noteCursor: Cursor?): ArrayList<Note>{
+        val noteList = ArrayList<Note>()
+
+        noteCursor?.apply {
+            while (moveToNext()){
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                noteList.add(Note(id, title, description, date))
+            }
+        }
+
+        return noteList
+    }
+
+    fun mapCursorToObject(noteCursor: Cursor?): Note {
+        var note = Note()
+        noteCursor?.apply {
+            moveToFirst()
+            val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            note = Note(id, title, description, date)
+        }
+        return note
+    }
+}
---
+++ consumerapp/src/main/java/com/genadidharma/consumerapp/ui/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.genadidharma.consumerapp.ui
+
+import android.content.Intent
+import android.database.ContentObserver
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.genadidharma.consumerapp.R
+import com.genadidharma.consumerapp.adapter.NoteAdapter
+import com.genadidharma.consumerapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.genadidharma.consumerapp.entity.Note
+import com.genadidharma.consumerapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
+    private lateinit var adapter: NoteAdapter
+//    private lateinit var noteHelper: NoteHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Consumer Notes"
+
+        rv_notes.layoutManager = LinearLayoutManager(this)
+        rv_notes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        rv_notes.adapter = adapter
+
+        fab_add.setOnClickListener {
+            val intent = Intent(this, NoteAddUpdateActivity::class.java)
+            startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+
+        val myObserver = object : ContentObserver(handler){
+            override fun onChange(selfChange: Boolean) {
+                loadNoteAsync()
+            }
+        }
+
+        /*noteHelper = NoteHelper.getInstance(applicationContext)
+        noteHelper.open()
+
+        loadNoteAsync()*/
+
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        if (savedInstanceState == null) {
+            loadNoteAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when (requestCode) {
+                NoteAddUpdateActivity.REQUEST_ADD -> if (requestCode == NoteAddUpdateActivity.REQUEST_ADD) {
+                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+
+                    adapter.addItem(note)
+                    rv_notes.smoothScrollToPosition(adapter.itemCount - 1)
+                    showSnackbarMessage("Satu item berhasil ditambahkan")
+                }
+                NoteAddUpdateActivity.REQUEST_UPDATE ->
+                    when (resultCode) {
+                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+                            val note =
+                                data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+
+                            adapter.updateItem(position, note)
+                            rv_notes.smoothScrollToPosition(position)
+                            showSnackbarMessage("Satu item berhasil diubah")
+                        }
+                        NoteAddUpdateActivity.RESULT_DELETE -> {
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+                            adapter.removeItem(position)
+                            showSnackbarMessage("Satu item berhasil dihapus")
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(rv_notes, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun loadNoteAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            progressbar.visibility = View.VISIBLE
+            val defferedNotes = async(Dispatchers.IO) {
+//                val cursor = noteHelper.queryAll()
+                val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            progressbar.visibility = View.INVISIBLE
+            val notes = defferedNotes.await()
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackbarMessage("Tidak ada data saat ini")
+            }
+        }
+    }
+}
